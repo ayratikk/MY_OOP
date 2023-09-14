794a3cfd0b33184c3aa57c5636037185cddb91d9
Айрат <ayratnasretdinov@gmail.com>
2023-09-14 22:35:10 +0300

    Extend classes

--- app/src/main/java/com/example/myoop/MainActivity.java
+++ app/src/main/java/com/example/myoop/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myoop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 Cat myCat;
@@ -11,11 +12,11 @@ Cat myCat;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myCat = new Cat(4,"Puma");
-
         myCat.talk();
 
         Cat murzik = new Cat(3, "Murza");
         murzik.talk();
+        murzik.breathe();
 
         Cat glasha = new Cat();
         glasha.initFields(5, "Glasha");
@@ -24,5 +25,14 @@ Cat myCat;
         Cat catt = new Cat();
         catt = new Cat(4,"Puma","birmanskaya","white");
         catt.talkAboutBreedAndColor();
+
+        Puma puma = new Puma();
+
+        puma.breathe();
+        puma.talk();
+        puma.isAlive = true;
+        puma.name = "Leo";
+        Log.i( "isAlive", String.valueOf(puma.isAlive));
+        Log.i( "breathe()", "My name is " + puma.name + " and " + puma.breathe());
     }
 }
---
+++ app/src/main/java/com/example/myoop/Puma.java
@@ -0,0 +1,5 @@
+package com.example.myoop;
+
+public class Puma extends Cat{
+
+}
