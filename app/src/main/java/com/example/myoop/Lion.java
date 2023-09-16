341d34832159e1a4c758d3b60d2372cea30faeda
Айрат <ayratnasretdinov@gmail.com>
2023-09-16 16:09:40 +0300

    Полиморфизм

---
+++ app/src/main/java/com/example/myoop/Lion.java
@@ -0,0 +1,9 @@
+package com.example.myoop;
+
+import android.util.Log;
+
+public class Lion extends Cat{
+    public void talk(){
+        Log.i("talk()", "RRRRR! I m lion. My name is " + name + ", and I'm " + age + " years old.");
+    }
+}
