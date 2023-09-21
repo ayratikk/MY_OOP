e91c21c2ceb5079210a6648ad1ee26e7f43e1cc7
Айрат <ayratnasretdinov@gmail.com>
2023-09-21 14:59:49 +0300

    Приведение типов

--- app/src/main/java/com/example/myoop/MainActivity.java
+++ app/src/main/java/com/example/myoop/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,16 +14,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Cat vaska = new Cat();
-        vaska.talk(3);
+        // vaska.talk(3);
 
         Puma bagira = new Puma();
-        bagira.talk();
+        // bagira.talk();
 
         Cat puma = new Puma();
-        puma.talk();
+        //puma.talk();
 
         Cat murzik = new Lion();
-        murzik.talk();
+        //lion.talk();
+
+        Cat lion = new Lion();
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        cats.add(vaska);
+        cats.add(bagira);
+        cats.add(puma);
+        cats.add(murzik);
+        cats.add(lion);
+
+        Cat cat = cats.get(0);
+        Cat cat1 = cats.get(1);
+        Cat cat2 = cats.get(2);
+        Cat cat3 = cats.get(3);
+        Cat cat4 = cats.get(4);
+        Puma puma1 = (Puma) cats.get(1);
+        puma1.talk();
+
+        Cat cat5 = cats.get(4);
+
+        if (cat5 instanceof Puma) {
+
+            Puma puma2 = (Puma) cat5;
+            puma2.talk();
+        } else {
+            Log.i("instanceof", "This is not Puma");
+        }
     }
 
 
